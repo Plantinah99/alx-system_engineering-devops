2e3b98f40b321e596bcb897b4945af485ed80a43
Plantinah99 <plantinahtshukudu@gmail.com>
2024-07-11 08:50:22 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers. Returns 0 if the subreddit is invalid.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyRedditBot/1.0'}
 
     try:
@@ -22,16 +22,6 @@ def number_of_subscribers(subreddit):
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
-        else:
-            return 0
+        return 0
     except Exception:
         return 0
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subscribers = number_of_subscribers(sys.argv[1])
-        print(subscribers)
