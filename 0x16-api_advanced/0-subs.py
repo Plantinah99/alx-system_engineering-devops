70b5c0ce8936a50c95d23770abea43cf62e8fa43
Plantinah99 <plantinahtshukudu@gmail.com>
2024-07-11 08:42:42 -0700

    task 0 mod

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,28 +14,17 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers. Returns 0 if the subreddit is invalid.
     """
-    # Reddit API URL for subreddit information
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': 'MyRedditBot/1.0'}
 
     try:
-        # Make the API request
         response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if the subreddit is valid
         if response.status_code == 200:
-            # Parse the JSON response
             data = response.json()
-            # Return the number of subscribers
             return data['data']['subscribers']
         else:
-            # Invalid subreddit
             return 0
-
-    except requests.RequestException:
-        # Handle any request-related errors
+    except Exception:
         return 0
 
 
@@ -44,4 +33,5 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subscribers = number_of_subscribers(sys.argv[1])
+        print(subscribers)
